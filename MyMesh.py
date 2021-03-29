d6a8b246bd4dad719db36658ad6c20f368f28a9b
PCaruana9 <caruana9@my.yorku.ca>
2021-03-29 04:03:29 +0000

    Wrapper Library for meshio

---
+++ MyMesh.py
@@ -0,0 +1,24 @@
+
+import meshio
+import numpy as np
+
+#Author: Peter Caruana
+#Email:  caruana9@my.yorku.ca
+
+# This function library exists as a wraper for meshio to implement PyMesh features
+# Why? Because pip install PyMesh doesnt give you the full version of PyMesh, and
+# god help you if you are trying to build and install PyMesh2 on google colab
+
+#takes meshio mesh as input
+#implements PyMesh bbox feature
+#returns bounding vertex coordinates of mesh
+#[0] are minimum values, [1] are maximum values
+def bbox(mesh):
+  columns = list(zip(*mesh.points))
+  max = [np.max(row) for row in columns]
+  min = [np.min(row) for row in columns]
+  
+  return np.array([min, max])
+
+def getFaces(mesh):
+  return mesh.cells[0][1]
